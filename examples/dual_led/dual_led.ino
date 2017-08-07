b32e9dd7f6c90169e4d02b298da438450987ea8e
Jan Delgado <jdelgado@gmx.net>
2017-08-07 21:20:05 +0200

    fixed linting error

--- examples/dual_led/dual_led.ino
+++ examples/dual_led/dual_led.ino
@@ -5,7 +5,7 @@
 // the LED connected to gpio 9 is in breathe mode, the builtin LED
 // LED is blinking with the same frequency.
 JLed led1 = JLed(9).Breathe(2000).Forever().DelayAfter(2000);
-JLed led2 = JLed(LED_BUILTIN).Blink(2000,2000).Forever();
+JLed led2 = JLed(LED_BUILTIN).Blink(2000, 2000).Forever();
 
 void setup() {
 }
