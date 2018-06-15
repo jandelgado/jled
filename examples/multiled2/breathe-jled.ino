ebe2870bceab1deffae08695af6baa5ca81e8a41
wd5gnr <al.williams@awce.com>
2018-06-15 13:36:18 -0500

    Reverted example to match others

--- examples/multiled2/breathe-jled.ino
+++ examples/multiled2/breathe-jled.ino
@@ -1,11 +1,8 @@
 #include <jled.h>
 
 
-// For some reason, making the setup calls here before elaboration completes
-// causes the this pointer in the JLed constructor to be wrong
-// I didn't track that down, but the answer is to make the calls post main.
-JLed led = JLed(11); //.Breathe(2000).DelayAfter(1000).Forever();
-JLed led1 = JLed(13); //.Blink(500,500).Forever();
+JLed led = JLed(11).Breathe(2000).DelayAfter(1000).Forever();
+JLed led1 = JLed(13).Blink(500,500).Forever();
 
 
 
@@ -13,16 +10,10 @@ JLed led1 = JLed(13); //.Blink(500,500).Forever();
 
 
 void setup() { 
-   led.Breathe(2000).DelayAfter(1000).Forever();
-   led1.Blink(500,500).Forever();
-
-
 }
 
 
 
 void loop() {
-  
-
   JLed::UpdateAll();
 } 
