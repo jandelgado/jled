054cd2f406d58ebd0f0bdfe3313e34731bd7fc52
wd5gnr <al.williams@awce.com>
2018-06-16 07:43:49 -0500

    Maybe last space commit for pendantic upstream lint

--- examples/multiled2/breathe-jled.ino
+++ examples/multiled2/breathe-jled.ino
@@ -1,18 +1,14 @@
+// JLed multi LED demo. control multiple LEDs in-sync.
+// Copyright 2017 by Jan Delgado, Al Williams. All rights reserved.
+// https://github.com/jandelgado/jled
+
 #include <jled.h>
 
-
 JLed led = JLed(11).Breathe(2000).DelayAfter(1000).Forever();
-JLed led1 = JLed(13).Blink(500,500).Forever();
-
-
-
-
-
+JLed led1 = JLed(13).Blink(500, 500).Forever();
 
 void setup() { 
 }
-
-
 
 void loop() {
   JLed::UpdateAll();
