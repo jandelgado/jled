be95157a83f8be5dfb72bb29b16be869e20b4175
wd5gnr <al.williams@awce.com>
2018-06-16 07:46:13 -0500

    One more white space pass... please

--- examples/multiled2/breathe-jled.ino
+++ examples/multiled2/breathe-jled.ino
@@ -7,9 +7,9 @@
 JLed led = JLed(11).Breathe(2000).DelayAfter(1000).Forever();
 JLed led1 = JLed(13).Blink(500, 500).Forever();
 
-void setup() { 
+void setup() {
 }
 
 void loop() {
   JLed::UpdateAll();
-} 
+}
