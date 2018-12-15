5734d531fc01ca35dcdddb3170c6ac17b47a3734
jdelgado <jdelgado@gmx.net>
2018-12-15 01:33:19 +0100

    refactor JLedSequence and add parallel processing of JLed array

--- test/test_jled_sequence.cpp
+++ test/test_jled_sequence.cpp
@@ -7,25 +7,40 @@
 // TODO(jd) implement test
 TEST_CASE("empty sequence returns false on Update()", "[jled_sequence]") {}
 
-TEST_CASE("simple sequence performs all updates", "[jled_sequence]") {
+TEST_CASE("sequential sequence performs all updates", "[jled_sequence]") {
     arduinoMockInit();
 
-    constexpr auto kPin = 1;
     JLed leds[] = {
         JLed(1).Blink(1, 1).Repeat(1),
-        JLed(1).Blink(1, 1).Repeat(1)
+        JLed(2).Blink(1, 1).Repeat(1).LowActive(),
     };
-    JLedSequence seq(leds, 2);
+    JLedSequence seq(JLedSequence::eMode::PARALLEL, leds, 2);
+    constexpr uint8_t expected1[] = {255, 0, 0};
+    constexpr uint8_t expected2[] = {0, 255, 255};
+
+    uint32_t time = 0;
+    for (size_t i = 0; i < 3; i++) {
+        seq.Update();
+        REQUIRE(expected1[i] == arduinoMockGetPinState(1));
+        REQUIRE(expected2[i] == arduinoMockGetPinState(2));
+        arduinoMockSetMillis(++time);
+    }
+}
+
+TEST_CASE("sequence performs all updates", "[jled_sequence]") {
+    arduinoMockInit();
+
+    JLed leds[] = {JLed(1).Blink(1, 1).Repeat(1),
+                   JLed(1).Blink(1, 1).Repeat(1)};
+    JLedSequence seq(JLedSequence::eMode::SEQUENCE, leds, 2);
     constexpr uint8_t expected[] = {255,  // first led
                                     0,
-                                    255,   // second led
-                                    0,
-                                    0};  // final state
-
+                                    255,    // second led
+                                    0, 0};  // final state
     uint32_t time = 0;
     for (const auto val : expected) {
         seq.Update();
-        REQUIRE(arduinoMockGetPinState(kPin) == val);
+        REQUIRE(val == arduinoMockGetPinState(1));
         arduinoMockSetMillis(++time);
     }
 }
