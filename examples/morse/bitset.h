42d4b9d2fbdfc780e5081c25dfe661f77fd29dca
jdelgado <jdelgado@gmx.net>
2019-01-02 22:38:38 +0100

    unit test for morse example

--- examples/morse/bitset.h
+++ examples/morse/bitset.h
@@ -3,18 +3,23 @@
 class Bitset {
  private:
     size_t n_;
-    uint8_t *bits_;
+    uint8_t* bits_;
 
  public:
     Bitset() = delete;
     Bitset(const Bitset&) = delete;
     Bitset(Bitset&&) = delete;
-    Bitset(size_t N) : n_(N), bits_{new uint8_t[((N-1)>>3)+1]} {
-        memset(bits_, 0, ((n_-1)>>3)+1);
+    Bitset(size_t N)
+        : n_(N), bits_{N > 0 ? new uint8_t[((N - 1) >> 3) + 1] : nullptr} {
+        if (n_) 
+            memset(bits_, 0, ((n_ - 1) >> 3) + 1);
     }
-    Bitset& operator = (const Bitset&) = delete;
-    Bitset& operator = (Bitset&&) = delete;
-    virtual ~Bitset() { delete [] bits_; bits_ = nullptr;}
+    Bitset& operator=(const Bitset&) = delete;
+    Bitset& operator=(Bitset&&) = delete;
+    virtual ~Bitset() {
+        delete[] bits_;
+        bits_ = nullptr;
+    }
     void set(size_t i, bool val) {
         if (val)
             bits_[i >> 3] |= (1 << (i & 7));
