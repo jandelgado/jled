0c656fbc3d41991a9cbb49c31c98431d4c775a1d
jandelgado <jandelgado@users.noreply.github.com>
2020-07-18 21:14:39 +0200

    add support for nano33ble using mbed arduino core (#54)
    The Arduino core for the nano33ble uses mbed and wraps it in the Arduino
    core API. Therefore __MBED__ is defined, which makes JLed think it runs
    in an Mbed environment. But in this case, JLed needs to use the Arduino
    API. The problem is solved by checking also for ARDUINO_API_VERSION:
      #if defined(__MBED__) && !defined(ARDUINO_API_VERSION)
      -> running in mbed environment
    in jled.h.
    
    See
     * https://github.com/arduino/ArduinoCore-API
     * https://github.com/arduino/ArduinoCore-nRF528x-mbedos
    
    Fixes #53

--- examples/sequence/sequence.ino
+++ examples/sequence/sequence.ino
@@ -3,15 +3,15 @@
 // https://github.com/jandelgado/jled
 #include <jled.h>
 
-constexpr auto PIN_LED = 3;
+constexpr auto LED_PIN = 3;
 
 JLed leds[] = {
-    JLed(PIN_LED).Breathe(2000).Repeat(3),
-    JLed(PIN_LED).Blink(750, 250).Repeat(3),
-    JLed(PIN_LED).FadeOff(1000).Repeat(3),
-    JLed(PIN_LED).Blink(500, 500).Repeat(3),
-    JLed(PIN_LED).FadeOn(1000).Repeat(3),
-    JLed(PIN_LED).Off()
+    JLed(LED_PIN).Breathe(2000).Repeat(3),
+    JLed(LED_PIN).Blink(750, 250).Repeat(3),
+    JLed(LED_PIN).FadeOff(1000).Repeat(3),
+    JLed(LED_PIN).Blink(500, 500).Repeat(3),
+    JLed(LED_PIN).FadeOn(1000).Repeat(3),
+    JLed(LED_PIN).Off()
 };
 
 JLedSequence sequence(JLedSequence::eMode::SEQUENCE, leds);
