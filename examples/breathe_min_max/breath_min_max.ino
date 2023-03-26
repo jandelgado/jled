7696bc89103215ae1be85d6ac66ffb5a0f5b0d86
Pieter <47089904+Welsyntoffie@users.noreply.github.com>
2023-03-26 06:48:10 +0200

    Update breath_min_max.ino

--- examples/breathe_min_max/breath_min_max.ino
+++ examples/breathe_min_max/breath_min_max.ino
@@ -7,6 +7,7 @@ int minbrightness = 10;
 int maxbrightness = 200;
 
 // breathe LED forever from minimum to maximum, LED is connected to pin 9 (PWM capable) gpio
+// LED will take 4 seconds to go from minimum to maximum and 4 seconds from maximum to minimum
 auto led = JLed(9).Breathe(4000).MinBrightness(minbrightness).MaxBrightness(maxbrightness).Forever();
 
 void setup() {
