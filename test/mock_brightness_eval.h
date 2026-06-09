27e06dd5727e88a91ea9d24a7ebe4f1bc2703b73
Jan Delgado <jandelgado@users.noreply.github.com>
2026-06-09 23:06:14 +0200

    refactor TJLedGroup tests

--- test/mock_brightness_eval.h
+++ test/mock_brightness_eval.h
@@ -1,4 +1,4 @@
-// Copyright 2017-2020 Jan Delgado jdelgado@gmx.net
+// Copyright 2017-2026 Jan Delgado jdelgado@gmx.net
 //
 #ifndef TEST_MOCK_BRIGHTNESS_EVAL_H_
 #define TEST_MOCK_BRIGHTNESS_EVAL_H_
@@ -8,24 +8,25 @@
 #include <cassert>
 #include <vector>
 
-using ByteVec = std::vector<uint8_t>;
-
-// a brightness evaluator used for the test. returns predefined values f(t)=y
-// for each point in time t.
-class MockBrightnessEvaluator : public jled::BrightnessEvaluator<uint8_t> {
-    ByteVec values_;
+// A brightness evaluator used for tests. Returns predefined values f(t)=y
+// for each point in time t. Templated on brightness type to support both
+// 8-bit (JLed) and 16-bit (JLedHD) LEDs.
+template <typename B>
+class MockBrightnessEvaluatorT : public jled::BrightnessEvaluator<B> {
+    std::vector<B> values_;
     mutable uint16_t count_ = 0;
 
  public:
-    explicit MockBrightnessEvaluator(ByteVec values) : values_(values) {}
+    explicit MockBrightnessEvaluatorT(std::vector<B> values) : values_(values) {}
     uint16_t TimesEvalWasCalled() const { return count_; }
     uint16_t Period() const { return values_.size(); }
-    uint8_t Eval(uint32_t t) const {
+    B Eval(uint32_t t) const {
         assert(t < values_.size());
         count_++;
         return values_[t];
     }
 };
 
-#endif  // TEST_MOCK_BRIGHTNESS_EVAL_H_
+using MockBrightnessEvaluator = MockBrightnessEvaluatorT<uint8_t>;
 
+#endif  // TEST_MOCK_BRIGHTNESS_EVAL_H_
